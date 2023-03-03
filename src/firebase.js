6f2dd8b0380c9ad76f676c2f662c865980350bf3
sanjayyadavgithub <48355724+sanjayyadavgithub@users.noreply.github.com>
2023-03-03 17:51:50 +0530

    firebase update

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { GoogleAuthProvider,FacebookAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAQitrmUuUc0BysETVOF_uZJIgiEaSV1DA",
-  authDomain: "react-firebase-auth-test-d03d7.firebaseapp.com",
-  projectId: "react-firebase-auth-test-d03d7",
-  storageBucket: "react-firebase-auth-test-d03d7.appspot.com",
-  messagingSenderId: "469794408622",
-  appId: "1:469794408622:web:76784522ea8738aca9053f"
+  apiKey: "key",
+  authDomain: "domin",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "1f"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,4 +18,4 @@ const facebookprovider = new FacebookAuthProvider();
 const googleprovider = new GoogleAuthProvider();
 
 
-export  {auth,facebookprovider,googleprovider}
+export  {auth,facebookprovider,googleprovideappId
